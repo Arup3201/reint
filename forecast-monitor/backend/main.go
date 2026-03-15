96c03f61516186e46d965c19cc52a35cb609dd8e
Arup3201 <arupjana7365@gmail.com>
2026-03-15 22:05:01 +0530

    backend added with /wind-data api

---
+++ forecast-monitor/backend/main.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"slices"
+	"strconv"
+	"time"
+)
+
+const (
+	host = "localhost"
+	port = "8080"
+)
+
+type WindData struct {
+	TargetTime  string  `json:"time"`
+	PublishTime string  `json:"publish_time"`
+	Actual      float64 `json:"actual"`
+	Forecast    float64 `json:"forecast"`
+}
+
+var (
+	DATASET = []WindData{}
+)
+
+func LoadDataset() error {
+
+	windApi := "https://data.elexon.co.uk/bmrs/api/v1/datasets/FUELHH/stream?settlementDateFrom=2024-01-01&settlementDateTo=2024-01-31&fuelType=WIND"
+	forecastApi := "https://data.elexon.co.uk/bmrs/api/v1/datasets/WINDFOR/stream?publishDateTimeFrom=2024-01-01&publishDateTimeTo=2024-01-31"
+
+	type apiData struct {
+		StartTime   string  `json:"startTime"`
+		PublishTime string  `json:"publishTime"`
+		Generation  float64 `json:"generation"` // MW
+	}
+
+	var req *http.Request
+	var res *http.Response
+	var err error
+
+	req, _ = http.NewRequest("GET", windApi, nil)
+	res, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("actual wind data response: %w", err)
+	}
+	defer res.Body.Close()
+
+	actualWindData := []apiData{}
+	if err = json.NewDecoder(res.Body).Decode(&actualWindData); err != nil {
+		return fmt.Errorf("actual wind data response decode: %w", err)
+	}
+
+	req, _ = http.NewRequest("GET", forecastApi, nil)
+	res, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("forecasted wind data response: %w", err)
+	}
+	defer res.Body.Close()
+
+	forecastedWindData := []apiData{}
+	if err := json.NewDecoder(res.Body).Decode(&forecastedWindData); err != nil {
+		return fmt.Errorf("forecasted wind data response decode: %w", err)
+	}
+
+	DATASET = []WindData{}
+	for _, actual := range actualWindData {
+		for _, forecast := range forecastedWindData {
+			if actual.StartTime == forecast.StartTime {
+				DATASET = append(DATASET, WindData{
+					TargetTime:  actual.StartTime,
+					PublishTime: forecast.PublishTime,
+					Actual:      actual.Generation,
+					Forecast:    forecast.Generation,
+				})
+			}
+		}
+	}
+
+	return nil
+}
+
+func getWindData(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	start := r.URL.Query().Get("start")
+	end := r.URL.Query().Get("end")
+	horizon := r.URL.Query().Get("horizon")
+
+	if start == "" || end == "" || horizon == "" {
+		http.Error(w, "Start, end or horizon value cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	var minTime, maxTime time.Time
+	minTime = time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+	maxTime = time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC)
+
+	var startTime time.Time
+	startTime, err = time.Parse(time.RFC3339, start)
+	if err != nil {
+		log.Printf("[ERROR] parse start time: %s\n", err)
+		http.Error(w, "Start time is not valid, valid format 2024-01-01T02:30:00Z", http.StatusBadRequest)
+		return
+	}
+
+	var endTime time.Time
+	endTime, err = time.Parse(time.RFC3339, end)
+	if err != nil {
+		log.Printf("[ERROR] parse end time: %s\n", err)
+		http.Error(w, "End time is not valid, valid format 2024-01-01T02:30:00Z", http.StatusBadRequest)
+		return
+	}
+
+	if minTime.Sub(startTime) > 0 || startTime.Sub(maxTime) >= 0 ||
+		minTime.Sub(endTime) > 0 || endTime.Sub(maxTime) >= 0 {
+		http.Error(w, "Only January 2024 values are accepted for start time and end time", http.StatusBadRequest)
+		return
+	}
+
+	if endTime.Sub(startTime) < 0 {
+		http.Error(w, "End time cannot be less than start time", http.StatusBadRequest)
+		return
+	}
+
+	var horizonInt int
+	horizonInt, err = strconv.Atoi(horizon)
+	if err != nil {
+		log.Printf("[ERROR] parse horizon: %s\n", err)
+		http.Error(w, "Forecast horizon should be an integer in range 0-48 Hrs", http.StatusBadRequest)
+		return
+	}
+
+	if horizonInt < 0 || horizonInt > 48 {
+		http.Error(w, "Forecast horizon should be an integer in range 0-48 Hrs", http.StatusBadRequest)
+		return
+	}
+
+	var parsedTargetTime, parsedPublishTime time.Time
+	var results = []WindData{}
+	for _, data := range DATASET {
+		parsedTargetTime, _ = time.Parse(time.RFC3339, data.TargetTime)
+		parsedPublishTime, _ = time.Parse(time.RFC3339, data.PublishTime)
+		if parsedTargetTime.Sub(startTime) >= 0 &&
+			endTime.Sub(parsedTargetTime) >= 0 &&
+			parsedTargetTime.Sub(parsedPublishTime) >= time.Duration(horizonInt)*time.Hour {
+			results = append(results, data)
+		}
+	}
+
+	var parseTimeA, parseTimeB time.Time
+	slices.SortFunc(results, func(a, b WindData) int {
+		parseTimeA, _ = time.Parse(time.RFC3339, a.PublishTime)
+		parseTimeB, _ = time.Parse(time.RFC3339, b.PublishTime)
+		return int(parseTimeA.Sub(parseTimeB))
+	})
+
+	if len(results) == 0 {
+		json.NewEncoder(w).Encode(map[string]any{
+			"data": results,
+		})
+		return
+	}
+
+	var filteredResults []WindData
+	i, k := 0, 0
+
+	filteredResults = append(filteredResults, results[i])
+	i += 1
+	k += 1
+	for ; i < len(results); i++ {
+		if filteredResults[k-1].TargetTime == results[i].TargetTime {
+			filteredResults[k-1] = results[i]
+		} else {
+			filteredResults = append(filteredResults, results[i])
+			k += 1
+		}
+	}
+
+	json.NewEncoder(w).Encode(map[string]any{
+		"data": filteredResults,
+	})
+}
+
+func main() {
+	var err error
+
+	err = LoadDataset()
+	if err != nil {
+		log.Fatalf("load dataset: %s\n", err)
+	}
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf("%s:%s", host, port),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 20 * time.Second,
+	}
+
+	http.HandleFunc("GET /wind-data", getWindData)
+
+	log.Printf("[INFO] Server starting at %s:%s\n", host, port)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("server listen and serve: %s\n", err)
+	}
+}
---
+++ forecast-monitor/backend/main_test.go
@@ -0,0 +1,338 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadDataset(t *testing.T) {
+	err := LoadDataset()
+
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, len(DATASET))
+
+	var cnt int
+
+	cnt = 0
+	for _, data := range DATASET {
+		if data.TargetTime == "2024-01-03T21:00:00Z" {
+			cnt += 1
+		}
+	}
+	assert.Equal(t, 24, cnt)
+
+	cnt = 0
+	for _, data := range DATASET {
+		if data.TargetTime == "2024-01-30T21:00:00Z" {
+			cnt += 1
+		}
+	}
+	assert.Equal(t, 24, cnt)
+}
+
+func TestWindData_GET(t *testing.T) {
+	log.SetOutput(io.Discard)
+
+	DATASET = []WindData{
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-03T21:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+	}
+
+	t.Run("should get wind data", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2024-01-02T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 200, res.StatusCode)
+	})
+
+	DATASET = []WindData{
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-01T21:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-01T21:30:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-01T22:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-01T22:30:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-01T23:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+		{
+			PublishTime: "2024-01-01T03:00:00Z",
+			TargetTime:  "2024-01-01T23:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+	}
+
+	t.Run("should get 5 results between start on 1st Jan and end on 2nd Jan with horizon 4", func(t *testing.T) {
+
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2024-01-02T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		var responseBody struct {
+			Data []WindData `json:"data"`
+		}
+		if err := json.NewDecoder(res.Body).Decode(&responseBody); err != nil {
+			assert.NoError(t, err)
+			return
+		}
+		assert.Equal(t, 5, len(responseBody.Data))
+	})
+
+	t.Run("should get 2 results between start on 1st Jan 21:00 to 21:30 with horizon 4", func(t *testing.T) {
+
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T21:00:00Z&end=2024-01-01T21:30:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		var responseBody struct {
+			Data []WindData `json:"data"`
+		}
+		if err := json.NewDecoder(res.Body).Decode(&responseBody); err != nil {
+			assert.NoError(t, err)
+			return
+		}
+		assert.Equal(t, 2, len(responseBody.Data))
+	})
+
+	DATASET = []WindData{
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-01T12:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+		{
+			PublishTime: "2024-01-01T03:00:00Z",
+			TargetTime:  "2024-01-01T12:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+	}
+	t.Run("should get 1 result with latest forecast at same target time", func(t *testing.T) {
+
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2024-01-02T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		var responseBody struct {
+			Data []WindData `json:"data"`
+		}
+		if err := json.NewDecoder(res.Body).Decode(&responseBody); err != nil {
+			assert.NoError(t, err)
+			return
+		}
+		assert.Equal(t, 1, len(responseBody.Data))
+	})
+
+	DATASET = []WindData{
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-01T12:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-01T12:30:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-01T13:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+	}
+	t.Run("should get 2 results with horizon 10", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2024-01-02T00:00:00Z&horizon=10", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		var responseBody struct {
+			Data []WindData `json:"data"`
+		}
+		if err := json.NewDecoder(res.Body).Decode(&responseBody); err != nil {
+			assert.NoError(t, err)
+			return
+		}
+		assert.Equal(t, 2, len(responseBody.Data))
+	})
+
+	DATASET = []WindData{
+		{
+			PublishTime: "2024-01-01T02:30:00Z",
+			TargetTime:  "2024-01-03T21:00:00Z",
+			Actual:      6945,
+			Forecast:    10629,
+		},
+	}
+
+	t.Run("should get bad request without start time", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?end=2024-01-02T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request without end time", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request without forecast horizon", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2024-01-02T00:00:00Z", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request for invalid start time", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00Z&end=2024-01-02T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request for start time in Feb 2024", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-02-01T00:00Z&end=2024-01-02T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request for start time in Dec 2023", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2023-12-30T00:00Z&end=2024-01-02T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request for invalid end time", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2024-01-02T00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request for end time in Feb 2024", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2024-02-02T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request for end time in Dec 2023", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2023-12-30T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request for invalid forecast horizon", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2024-01-02T00:00:00Z&horizon=y", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request for forecast horizon 60", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-01T00:00:00Z&end=2024-01-02T00:00:00Z&horizon=60", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+	t.Run("should get bad request for start time more than end time", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/wind-data?start=2024-01-03T00:00:00Z&end=2024-01-02T00:00:00Z&horizon=4", nil)
+		w := httptest.NewRecorder()
+		getWindData(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, 400, res.StatusCode)
+	})
+}
